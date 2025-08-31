b71364b476fb7f9afd0941949e1231a9ce4fe3fe
Maximilian Pöllinger <maxi.poellinger@gmail.com>
2025-08-31 16:53:17 +0000

    testing: use test_repeated to execute a test a given number of  times, and collect the results

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,2 +1,24 @@
+use log::trace;
+
 mod droptest;
 pub use droptest::DropTest;
+
+pub fn test_repeated<F, R>(name: &str, iterations: usize, test: F) -> Vec<R>
+where
+    F: Fn(usize) -> R,
+{
+    assert!(
+        iterations > 0,
+        "Test {name} has to be repeated at least once!"
+    );
+    (0..iterations).map(|iteration| {
+        trace!(
+            "---------------------- {name} Test Iteration {iteration} start ----------------------"
+        );
+        let res = test(iteration);
+        trace!(
+            "---------------------- {name} Test Iteration {iteration} end ----------------------"
+        );
+        res
+    }).collect()
+}
